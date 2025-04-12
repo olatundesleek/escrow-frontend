fd59820702608f16856db4cefbb5e301653e864a
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-12 21:23:04 +0100

    Build of button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,35 @@
+import {ReactNode} from "react";
+
+//Documentation on how to use some styles like all as a default value buh perhaps you want to adjusts some you have room to do so and you can so pass function if required
+// if u encounter an issue console.bol()
+
+interface ButtonProps {
+  children: ReactNode;
+  color?: string;
+  textSize?: string;
+  padding?: string;
+  type?: "button" | "submit" | "reset";
+
+  onClick?: () => void;
+}
+
+const Button = ({
+  children,
+  color = "bg-[#005F73] text-white",
+  textSize = "font-2xl",
+  padding = "px-5 py-3",
+  onClick,
+type ="button"
+}: ButtonProps) => {
+  return (
+    <button
+    type={type}
+      onClick={onClick}
+      className={`${padding} ${textSize} ${color} rounded-md hover:opacity-80 transition duration-300`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
