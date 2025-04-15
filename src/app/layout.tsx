76cc853a3554518200b08b2faaf466ff51d06a35
Reward Steve <rewardstephen30@gmail.com>
2025-04-15 04:06:52 +0100

    ft-footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
+import Footer from "./components/Footer";
 import "./globals.css";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Escrow App",
   description: "Hey there",
 };
 
-export default function RootLayout({children}: {children: ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
