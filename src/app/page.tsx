574b980ed4f436c6d210d7cb5d55ee4f293ea3e3
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-16 12:16:41 +0000

    rectifying article mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import Header from "./components/Header";
-import Article from "./components/Article";
+// import Header from "./components/Header";
+// import Article from "./components/Article";
 
 const Home = () => {
   return (
     <main>
-      <Header>Our Commitment to Transparency and Security in Payments</Header>
-      <Article>
-        In today digital age, ensuring the security and transparency of payment
-        systems is paramount. At our company, we are dedicated to providing a
-        payment solution that not only meets but exceeds industry standards for
-        safety and reliability.
-      </Article>
+        
     </main>
   );
 };
