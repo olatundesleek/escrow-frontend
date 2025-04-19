734e02f22dca57908cc4e6a79a2bd812d16cd950
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-19 22:41:04 +0000

    Adding feature components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,11 @@
-<<<<<<< HEAD
-import HomepageBanner from './components/HomepageBanner';
-import Features from './components/Features';
-=======
-// import Header from "./components/Header";
-// import Article from "./components/Article";
-import AboutusCard from "./components/AboutusCard";
-import WorkflowCard from "./components/WorkflowCard";
->>>>>>> 01c59860436f96e20d9fb47f3fce9ad2d4b8b8fc
+import HomepageBanner from "./components/HomepageBanner";
 
 const Home = () => {
   return (
     <main>
-<<<<<<< HEAD
       <HomepageBanner />
-      <Features />
-=======
-      <AboutusCard
-        image="/secure.png"
-        title="Secure Transactions"
-        description="Stay secure with advanced encryption and protection."
-      />
-
-      <WorkflowCard
-        image="/greet.png"
-        title="Agreement Between Buyer and Seller"
-        number="1"
-        description="The Agreement Between Buyer and Seller for an escrow system ensures a secure and fair transaction by involving an escrow service provider as a neutral third party"
-      />
-      <WorkflowCard
-        style="row-reverse"
-        image="/payment.png"
-        title="Transfer Payment to Escrow Account"
-        number="2"
-        description="In an escrow system, the transfer of payment to an escrow account involves the buyer depositing the agreed-upon funds into a secure account managed by a neutral third party, the escrow service provider."
-      />
-      <WorkflowCard
-        image="/goods.png"
-        title="Provide Goods or Services to Buyer"
-        number="3"
-        description="In an escrow system, after the buyer has transferred the payment to the escrow account, the seller is responsible for delivering the agreed-upon goods or services to the buyer."
-      />
-      <WorkflowCard
-        style="row-reverse"
-        image="/funds.png"
-        title="Agreement Between Buyer and Seller"
-        number="4"
-        description="In an escrow system, releasing funds to the seller occurs after the buyer has received and approved the goods or services as per the agreed terms."
-      />
->>>>>>> 01c59860436f96e20d9fb47f3fce9ad2d4b8b8fc
     </main>
   );
 };
 
 export default Home;
-  
