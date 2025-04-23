cc42cc92751f28a46a978098a21210b9c38a6b74
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-23 18:22:38 +0000

    Adding the Subscribe and services components
    	modified:   src/app/components/ServiceCard.tsx
    	modified:   src/app/components/Subscribe.tsx
    	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import HomepageBanner from "./components/HomepageBanner";
-import Section from "./components/Section";
-import Workflow from "./components/Workflow";
-import Features from "./components/Features";
-import Faqs from "./components/Faqs";
-import Subscribe from "./components/Subscribe";
-import Services from "./components/Services";
-import { Testimonial } from "./components/Testimonial";
+import HomepageBanner from './components/HomepageBanner';
+import Section from './components/Section';
+import Workflow from './components/Workflow';
+import Features from './components/Features';
+import Faqs from './components/Faqs';
+import Subscribe from './components/Subscribe';
+import Services from './components/Services';
+import { Testimonial } from './components/Testimonial';
 
 const Home = () => {
   return (
