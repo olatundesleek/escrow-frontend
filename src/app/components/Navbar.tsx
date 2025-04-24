5604ecfaa8d206747221825d18f0e24f62d4e02c
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-04-24 16:55:20 +0100

    added fixed nav and workflow and features section
    	new file:   src/app/_context/StickyContext.tsx
    	new file:   src/app/_hooks/useIntersectionObserver.ts
    	new file:   src/app/_hooks/useNavbarHeight.ts
    	modified:   src/app/aboutus/page.tsx
    	modified:   src/app/blog/page.tsx
    	modified:   src/app/components/HomepageBanner.tsx
    	modified:   src/app/components/NavItem.tsx
    	modified:   src/app/components/Navbar.tsx
    	modified:   src/app/components/PageBanner.tsx
    	new file:   src/app/components/Workflow.tsx
    	modified:   src/app/components/WorkflowCard.tsx
    	new file:   src/app/constants/workFlow.ts
    	modified:   src/app/contact/page.tsx
    	modified:   src/app/globals.css
    	modified:   src/app/layout.tsx
    modified:   src/app/page.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,36 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Logo from './Logo';
 import MenuButton from './MenuButton';
 import NavMenu from './NavMenu';
+import { usePathname } from 'next/navigation';
+import { useStickyContext } from '../_context/StickyContext';
+import useNavbarHeight from '../_hooks/useNavbarHeight';
 
 export default function Navbar() {
   const [isToggled, setIsToggled] = useState<boolean>(false);
+  const pathname = usePathname();
+  const { isIntersecting } = useStickyContext();
+  const navRef = useNavbarHeight();
+
+  useEffect(
+    function () {
+      setIsToggled(false);
+    },
+
+    [pathname],
+  );
 
   return (
     <nav
-      className={`w-full absolute top-0 left-0 lg:relative lg:flex items-center justify-around bg-primary text-white px-4 py-2  lg:p-6 lg:px-32 lg:py-6 border-b border-secondary gap-4 z-50`}
+      ref={navRef}
+      className={`${
+        !isIntersecting
+          ? 'fixed shadow-md bg-amber-50 text-gray-600'
+          : 'absolute lg:relative  bg-primary text-amber-100'
+      } transition-all duration-500 ease-in-out w-full top-0 left-0 lg:flex items-center justify-around  px-4 py-2 lg:p-6 lg:px-32 lg:py-6 border-b border-secondary gap-4 z-50 `}
     >
       <div className='grow-8 flex justify-between items-center lg:mb-0'>
         <Logo />
@@ -24,3 +43,4 @@ export default function Navbar() {
     </nav>
   );
 }
+
