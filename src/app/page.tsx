8bb54173e9f4c76374c6aaebb15afeec9b66d3e9
Reward Steve <rewardstephen30@gmail.com>
2025-04-29 00:18:12 +0100

    modified:testimonail-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-import { Testimonial } from "./components/Testimonial";
-import Features from "./components/Features";
 import HomepageBanner from "./components/HomepageBanner";
 import Section from "./components/Section";
 import Workflow from "./components/Workflow";
-=======
-import HomepageBanner from './components/HomepageBanner';
-import Section from './components/Section';
-import Workflow from './components/Workflow';
-import Features from './components/Features';
-import Faqs from './components/Faqs';
-import Subscribe from './components/Subscribe';
-import Services from './components/Services';
->>>>>>> e668c979cb6836b99baa02d38596ecddfb60efe0
+import Features from "./components/Features";
+import Faqs from "./components/Faqs";
+import Subscribe from "./components/Subscribe";
+import Services from "./components/Services";
+import { Testimonial } from "./components/Testimonial";
 
 const Home = () => {
   return (
