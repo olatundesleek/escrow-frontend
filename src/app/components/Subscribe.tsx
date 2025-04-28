cae00fae8fa390bbe8be3d2577ddd2dad0d8593f
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-04-28 18:55:36 +0100

    added subscribe and services section

--- src/app/components/Subscribe.tsx
+++ src/app/components/Subscribe.tsx
@@ -4,23 +4,23 @@ import { FiArrowUpRight } from "react-icons/fi";
 
 const Subscribe = () => {
   return (
-    <section className="bg-[radial-gradient(circle_at_50%,_#000000,_#000000_50%,_#061207_75%,_#001204_75%)] mt-4 flex flex-col gap-6 justify-center items-center lg:p-30 p-20">
+    <section className='bg-[radial-gradient(circle_at_50%,_#000000,_#000000_50%,_#061207_75%,_#001204_75%)] mt-4 flex flex-col gap-6 justify-center items-center lg:p-30 p-20'>
       <SectionalTitle
-        title="SUBSCRIBE"
-        description="Stay Informed and Connected by Subscribing to Our Newsletter"
-        desSize="lg:text-3xl text-white"
+        title='SUBSCRIBE'
+        description='Stay Informed and Connected by Subscribing to Our Newsletter'
+        desSize='lg:text-3xl text-white'
       />
 
-      <div className="lg:flex w-auto justify-center items-center block border-2 border-green-400">
+      <div className='lg:flex w-full justify-center items-center flex flex-col lg:flex-row'>
         <input
-          className="lg:w-[50rem] border-0 lg:m-5 m-2 rounded-md outline-secondary h-13 text-xl bg-white p-8"
-          type="email"
-          placeholder="Enter Your Email Address"
+          className='lg:w-[50rem] lg:m-5 m-2 rounded-md outline-secondary h-13 text-xl bg-white p-8 border'
+          type='email'
+          placeholder='Enter Your Email Address'
         />
         <Button
-          color="bg-orange-400 hover:opacity-90 text-white"
-          textSize="text-2xl"
-          style="flex gap-3 items-center ml-20"
+          color='bg-orange-400 hover:opacity-90 text-white'
+          textSize='text-2xl'
+          style='flex gap-3 items-center lg:py-4'
         >
           Subscribe
           <span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Section from './components/Section';
 import Workflow from './components/Workflow';
 import Features from './components/Features';
 import Faqs from './components/Faqs';
+import Subscribe from './components/Subscribe';
+import ServiceCards from './components/ServiceCards';
+import Services from './components/Services';
 
 const Home = () => {
   return (
@@ -13,6 +16,12 @@ const Home = () => {
       </Section>
       <Section>
         <Features />
+      </Section>
+      <Section>
+        <Subscribe />
+      </Section>
+      <Section>
+        <Services />
       </Section>
       <Section>
         <Faqs />
