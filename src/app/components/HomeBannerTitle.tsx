a05a3ae375c353990ba91228c60ed481b19d4f94
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-30 16:56:56 +0000

    fixed homebanner

--- src/app/components/HomeBannerTitle.tsx
+++ src/app/components/HomeBannerTitle.tsx
@@ -9,10 +9,6 @@ const HomeBannerTitle = () => {
       <div className="flex gap-4 px-4 py-2 items-center sm:px-6 md:px-8 lg:px-12 justify-center w-full lg:justify-start">
         <div className="w-4 h-12 sm:w-6 sm:h-16 md:w-8 md:h-20 lg:w-8 lg:h-28 rounded-2xl bg-yellow-500"></div>
         <h2 className="text-xl sm:text-2xl md:text-3xl font-bold bg-gradient-to-r from-green-500 to-yellow-400 bg-clip-text text-transparent text-center lg:text-left">
-    <div className="bg-transparent h-auto w-full">
-      <div className="flex gap-4 px-4 py-2 items-center sm:px-6 md:px-8 lg:px-12 justify-center w-full lg:justify-start">
-        <div className="w-4 h-12 sm:w-6 sm:h-16 md:w-8 md:h-20 lg:w-8 lg:h-28 rounded-2xl bg-yellow-500"></div>
-        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold bg-gradient-to-r from-green-500 to-yellow-400 bg-clip-text text-transparent text-center lg:text-left">
           Your Financial Protector
         </h2>
       </div>
