95a701686fb9632b7c79cc3f96123d486ec847e6
Reward Steve <rewardstephen30@gmail.com>
2025-05-06 14:57:04 +0100

    update: user authentication pages

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,8 @@
+import Signup from "../components/Signup";
 export default function Page() {
   return (
-    <section className='w-full flex justify-center items-center'>
-      Sign Up
+    <section className="w-full flex justify-center items-center">
+      <Signup />
     </section>
   );
 }
