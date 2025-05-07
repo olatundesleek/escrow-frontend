ef889c3b11ba1720f9233ed8500fdf61f493b947
Reward Steve <rewardstephen30@gmail.com>
2025-05-07 11:04:02 +0100

    update: ft-login

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -40,8 +40,6 @@ export default function Login() {
   const loginContent = (
     <>
       {error && <p className="text-red-500 font-medium mb-4">{error}</p>}
-      {loading && <p className="text-gray-600 mb-4">Logging in...</p>}
-
       <div className="w-full">
         <label className="block pt-4 font-medium">
           Username or Email Address
@@ -76,9 +74,9 @@ export default function Login() {
         {loading ? "Logging in..." : "Log In"}
       </Button>
 
-      <div>
-        {"Don't have any account?"}
-        <Link href="#" className="text-secondary">
+      <div className="flex w-full justify-evenly p-2">
+        <p>{"Don't have any account ?"}</p>
+        <Link href="/register" className="text-secondary">
           Create Account
         </Link>
       </div>
