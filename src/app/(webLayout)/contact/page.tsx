f129d23f63a2cd95f129545fe1e6753a521b9c48
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-05-09 16:49:33 +0000

    Adding the chart and table component

--- src/app/(webLayout)/contact/page.tsx
+++ src/app/(webLayout)/contact/page.tsx
@@ -1,7 +1,9 @@
-import PageBanner from '../../components/PageBanner';
-import PageBannerTitle from '../../components/PageBannerTitle';
-import ContentForm from '../../components/ContentForm';
-import Section from '../../components/Section';
+import PageBanner from "../../components/PageBanner";
+import PageBannerTitle from "../../components/PageBannerTitle";
+import ContentForm from "../../components/ContentForm";
+import Section from "../../components/Section";
+import TransactionChart from "../../components/TransactionChart";
+import TransactionTable from "../../components/TransactionTable";
 
 export default function Page() {
   return (
@@ -12,6 +14,19 @@ export default function Page() {
       <Section>
         <ContentForm />
       </Section>
+      {/* <div className="flex lg:flex-row flex-col gap-6 p-6 border-2 justify-between">
+        <TransactionChart />
+        <TransactionTable />
+      </div> */}
+
+      <div className="flex flex-col lg:flex-row gap-6 p-6 border-2 border-gray-300">
+        <div className="w-full lg:w-1/2">
+          <TransactionChart />
+        </div>
+        <div className="w-full lg:w-1/2">
+          <TransactionTable />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/app/components/TransactionChart.tsx
@@ -0,0 +1,53 @@
+
+"use client";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+interface MonthlyData {
+  name: string;
+  deposit: number;
+  withdraw: number;
+}
+
+const data: MonthlyData[] = [
+  { name: "August-2024", deposit: 950, withdraw: 150 },
+  { name: "September-2024", deposit: 1200, withdraw: 150 },
+  { name: "October-2024", deposit: 0, withdraw: 350 },
+];
+
+const TransactionChart = () => {
+  return (
+    <div>
+      <h2 className="text-lg font-semibold text-gray-700 mb-4">
+        Deposit & Withdrawal
+      </h2>
+      <div className="bg-white h-full overflow-hidden flex flex-col border-2 border-black rounded-lg shadow-md">
+        <h2 className="bg-gray-300 p-4 text-sm  text-gray-700 border-b">
+          Progress Report for Last 12 Months
+        </h2>
+        <div className="flex-1 p-4">
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart data={data}>
+              <XAxis dataKey="name" tick={{ fontSize: 12, fill: "#333" }} />
+              <YAxis tick={{ fontSize: 12, fill: "#333" }} />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="deposit" fill="#A3E635" name="Total Deposit" />
+              <Bar dataKey="withdraw" fill="#FACC15" name="Total Withdraw" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionChart;
