3b2b231fc54381dda637abc08a84899762644428
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-05-09 17:56:19 +0000

    rectified some mistakes

--- src/app/(webLayout)/contact/page.tsx
+++ src/app/(webLayout)/contact/page.tsx
@@ -2,8 +2,7 @@ import PageBanner from "../../components/PageBanner";
 import PageBannerTitle from "../../components/PageBannerTitle";
 import ContentForm from "../../components/ContentForm";
 import Section from "../../components/Section";
-import TransactionChart from "../../components/TransactionChart";
-import TransactionTable from "../../components/TransactionTable";
+
 
 export default function Page() {
   return (
@@ -14,19 +13,9 @@ export default function Page() {
       <Section>
         <ContentForm />
       </Section>
-      {/* <div className="flex lg:flex-row flex-col gap-6 p-6 border-2 justify-between">
-        <TransactionChart />
-        <TransactionTable />
-      </div> */}
+      
 
-      <div className="flex flex-col lg:flex-row gap-6 p-6 border-2 border-gray-300">
-        <div className="w-full lg:w-1/2">
-          <TransactionChart />
-        </div>
-        <div className="w-full lg:w-1/2">
-          <TransactionTable />
-        </div>
-      </div>
+   
     </>
   );
 }
--- src/app/components/TransactionChart.tsx
+++ src/app/components/TransactionChart.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import {
@@ -22,6 +21,24 @@ const data: MonthlyData[] = [
   { name: "September-2024", deposit: 1200, withdraw: 150 },
   { name: "October-2024", deposit: 0, withdraw: 350 },
 ];
+
+{
+  /* <div className="flex flex-col lg:flex-row gap-6 p-6 border-2 border-gray-300">
+        <div className="w-full lg:w-1/2">
+          <TransactionChart />
+        </div>
+        <div className="w-full lg:w-1/2">
+          <TransactionTable />
+        </div>
+      </div> */
+}
+
+{
+  /* <div className="flex lg:flex-row flex-col gap-6 p-6 border-2 justify-between">
+        <TransactionChart />
+        <TransactionTable />
+      </div> */
+}
 
 const TransactionChart = () => {
   return (
