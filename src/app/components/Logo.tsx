16ec40d945b3ca283edb001562177084f592ac99
Reward Steve <rewardstephen30@gmail.com>
2025-05-12 13:46:36 +0100

    fixed background-height

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Logo({ logo = 'Escrow' }: { logo?: string }) {
+export default function Logo({ logo = "Escrow" }: { logo?: string }) {
   return (
-    <Link href='/' className='flex items-center'>
-      <h1 className='text-3xl font-bold text-secondary'>{logo}</h1>
+    <Link href="/" className="flex items-center">
+      <h1 className="text-3xl font-bold text-secondary">{logo}</h1>
     </Link>
   );
 }
