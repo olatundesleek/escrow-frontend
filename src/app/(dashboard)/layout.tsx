9f439d3057504c2d13acfff7a7b42b7301c2691e
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-05-19 18:38:46 +0100

    implemented authorization and protected routes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,3 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { useAuthContext } from '../_context/AuthContext';
-import { useEffect } from 'react';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { isUserAuthenticated } = useAuthContext();
-  const { push } = useRouter();
-
-  useEffect(() => {
-    if (!isUserAuthenticated) {
-      push('/login');
-    }
-  }, [isUserAuthenticated, push]);
-
-  if (!isUserAuthenticated) {
-    return null;
-  }
-
-  return <div>{children}</div>;
+  return children;
 }
