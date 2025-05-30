8ded63428132a83d0087c492c5a6a67442feb50a
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-05-30 16:47:19 +0100

    setup react query for remote state management for users dashboard and admin dashboard

---
+++ src/app/_components/AdminDashboard.tsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='text-4xl font-bold'>Admin Dashboard</h1>
+      <p className='mt-4 text-lg'>Welcome to the admin dashboard!</p>
+    </div>
+  );
+}
