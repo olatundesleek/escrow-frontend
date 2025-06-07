5b28a67b1d80957a80f98e89a3363e80b8c7861a
Reward Steve <rewardstephen30@gmail.com>
2025-06-07 17:17:18 +0100

    feat: add maintenance page component with ui and message

--- src/app/_context/SiteSettingContext.tsx
+++ src/app/_context/SiteSettingContext.tsx
@@ -9,6 +9,7 @@ import React, {
 } from "react";
 import { SiteSettingData } from "../_types/siteSetting";
 import Spinner from "../_components/Spinner";
+import { useRouter } from "next/navigation";
 
 interface SiteSettingContextProps {
   success: boolean;
@@ -34,6 +35,7 @@ export const SiteSettingProvider = ({ children }: { children: ReactNode }) => {
   const [success, setSuccess] = useState(false);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchSettings = async () => {
@@ -48,6 +50,11 @@ export const SiteSettingProvider = ({ children }: { children: ReactNode }) => {
         );
 
         const result = await res.json();
+        if (result.maintenanceMode.enabled === true) {
+          router.push("/maintenance");
+        } else {
+          router.push("/");
+        }
         if (!res.ok) {
           setSuccess(false);
           setMessage(result.message || "Failed to load site settings");
@@ -66,7 +73,7 @@ export const SiteSettingProvider = ({ children }: { children: ReactNode }) => {
     };
 
     fetchSettings();
-  }, []);
+  }, [router]);
 
   if (loading) {
     return (
