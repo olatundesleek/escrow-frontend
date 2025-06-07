7365c77f5fbe53f9ab3679102a987c635466c9d9
Reward Steve <rewardstephen30@gmail.com>
2025-06-07 18:36:19 +0100

    feat: modified-ui

--- src/app/_components/ForgottenPassword.tsx
+++ src/app/_components/ForgottenPassword.tsx
@@ -35,6 +35,7 @@ export default function ForgottenPasssword() {
     <>
       <div className="w-full">
         <AuthInput
+          placeholder="youremail@email.com"
           InputTitle="Username or Email Address"
           type="email"
           value={email}
