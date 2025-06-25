d10558d928a9ebadca6f44443fdf8e4dc37de47a
Reward Steve <rewardstephen30@gmail.com>
2025-06-25 13:31:38 +0100

    wallet-color-map

--- src/app/_components/WalletCard.tsx
+++ src/app/_components/WalletCard.tsx
@@ -1,23 +1,31 @@
+import { walletColorMap } from "../_constants/wallet";
 import { useCardData } from "../_hooks/useCardData";
 
 export const WalletCard = () => {
   const { cardData } = useCardData();
+  console.log(cardData);
   return (
     <section className="grid sm:grid-cols-3 gap-6 my-8">
-      {cardData.map(({ label, value, icon: Icon, color }) => (
-        <div
-          key={label}
-          className={`bg-gradient-to-tr from-${color}-100 to-${color}-50 shadow-md rounded-2xl p-6 flex items-center gap-4`}
-        >
-          <Icon className={`text-${color}-600 text-3xl`} />
-          <div>
-            <h4 className={`text-sm font-medium text-${color}-800`}>{label}</h4>
-            <p className={`text-xl font-bold text-${color}-900`}>
-              ₦{value.toLocaleString()}
-            </p>
+      {cardData.map(({ label, value, icon: Icon, color }) => {
+        const style = walletColorMap[color];
+        return (
+          <div
+            key={label}
+            className={`bg-gradient-to-tr ${style.bg} shadow-md rounded-2xl p-6 flex items-center gap-4`}
+          >
+            <Icon className={`${style.icon} text-3xl`} />
+            <div>
+              <h4 className={`text-sm font-medium ${style.title}`}>{label}</h4>
+              <p
+                className={`text-xl font-bold ${style.text}`}
+                style={{ color: color }}
+              >
+                ₦{value.toLocaleString()}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </section>
   );
 };
