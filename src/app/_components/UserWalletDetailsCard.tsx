7877e90e8b0c2f61981ea0bfab0a078c7de0afe6
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-10-15 18:54:45 +0100

    updated wallet cards

--- src/app/_components/UserWalletDetailsCard.tsx
+++ src/app/_components/UserWalletDetailsCard.tsx
@@ -8,27 +8,27 @@ interface Props {
 }
 export default function UserWalletDetailsCard({ wallet }: Props) {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+    <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3'>
       <WalletDetailsCard
-        title="Total Balance"
+        title='Total Balance'
         value={wallet.totalBalance || 0}
-        color={"text-green-800"}
-        border="border border-green-200"
-        icon={<TbWallet className="text-green-600" />}
+        color={'text-blue-800'}
+        border='border border-blue-200'
+        icon={<TbWallet className='text-blue-600' />}
       />
       <WalletDetailsCard
-        title="Available Balance"
+        title='Available Balance'
         value={wallet.availableBalance || 0}
-        color={"text-blue-800"}
-        border="border border-blue-200"
-        icon={<LuPiggyBank className="text-blue-600" />}
+        color={'text-green-800'}
+        border='border border-green-200'
+        icon={<LuPiggyBank className='text-green-600' />}
       />
       <WalletDetailsCard
-        title="Locked Balance"
+        title='Locked Balance'
         value={wallet.lockedBalance || 0}
-        color={"text-red-800"}
-        border=" border border-red-200"
-        icon={<TbWalletOff className="text-red-600" />}
+        color={'text-red-800'}
+        border=' border border-red-200'
+        icon={<TbWalletOff className='text-red-600' />}
       />
     </div>
   );
--- src/app/_components/WalletCard.tsx
+++ src/app/_components/WalletCard.tsx
@@ -5,25 +5,25 @@ import { FaChartLine, FaMoneyBill, FaPiggyBank } from "react-icons/fa";
 export const WalletCard = ({ walletData }: { walletData: UserBaseWallet }) => {
   const cardData = [
     {
-      label: "Available Balance",
+      label: 'Available Balance',
       value: walletData.availableBalance,
-      icon: <FaMoneyBill className="text-blue-500" />,
-      color: "text-blue-700",
-      border: "border border-green-200",
+      icon: <FaMoneyBill className='text-green-500' />,
+      color: 'text-green-700',
+      border: 'border border-green-200',
     },
     {
-      label: "Locked Amount",
+      label: 'Locked Amount',
       value: walletData.lockedBalance,
-      icon: <FaPiggyBank className="text-red-500" />,
-      color: "text-red-700",
-      border: "border border-red-200",
+      icon: <FaPiggyBank className='text-red-500' />,
+      color: 'text-red-700',
+      border: 'border border-red-200',
     },
     {
-      label: "Total Amount",
+      label: 'Total Amount',
       value: walletData.totalBalance,
-      icon: <FaChartLine className="text-green-500" />,
-      color: "text-green-700",
-      border: "border border-blue-200",
+      icon: <FaChartLine className='text-blue-500' />,
+      color: 'text-blue-700',
+      border: 'border border-blue-200',
     },
   ];
 
